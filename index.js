3b2ddefd07f06ffdbf67e9f2755a9754b559b8cd
sazzad <sajusajjad052@gmail.com>
2022-10-25 21:23:16 +0600

    'load'

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const tech = require("./data/tech.json");
+const techDetail = require("./data/details.json");
+
+app.get("/", (req, res) => {
+  res.send("Hello World! d");
+});
+
+app.get("/tech", (req, res) => {
+  res.send(tech);
+});
+
+app.get("/technology/:id", (req, res) => {
+  const id = req.params.id;
+  const detail = techDetail.find((n) => n._id === id);
+  res.send(detail);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
